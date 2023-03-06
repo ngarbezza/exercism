cfec4f2e266487035f45ba7109392f8aa775fc76
Nahuel Garbezza <n.garbezza@gmail.com>
2023-03-06 19:16:14 -0300

    feat(rust): assembly line exercise, second iteration

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,17 +1,17 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    let success_rate_100_range = 1..=4;
-    let success_rate_90_range = 5..=8;
-    let success_rate_77_range = 9..=10;
+    let success_rate;
     let cars_per_hour_at_100_success = 221.0;
-    if success_rate_100_range.contains(&speed) {
-      speed as f64 * cars_per_hour_at_100_success * 1.0
-    } else if success_rate_90_range.contains(&speed) {
-      speed as f64 * cars_per_hour_at_100_success * 0.9
-    } else if success_rate_77_range.contains(&speed) {
-      speed as f64 * cars_per_hour_at_100_success * 0.77
+    if (1..=4).contains(&speed) {
+      success_rate = 1.0
+    } else if (5..=8).contains(&speed) {
+      success_rate = 0.9
+    } else if (9..=10).contains(&speed) {
+      success_rate = 0.77
     } else {
-      0.0
+      success_rate = 0.0
     }
+
+    speed as f64 * cars_per_hour_at_100_success * success_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
